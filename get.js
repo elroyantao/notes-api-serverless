001bd218df970c792c521aac75646d34d00adf90
Elroy Antao <elroyantao@gmail.com>
2020-02-09 01:49:05 +0000

    get API

--- get.js
+++ get.js
@@ -1,24 +1,24 @@
-import uuid from 'uuid'
 import * as dynamoDbLib from './libs/dynamodb-lib'
 import { success, failure } from './libs/response-lib'
 
 export async function main (event) {
-  const data = JSON.parse(event.body)
-
   const params = {
     TableName: process.env.tableName,
-    Item: {
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      noteId: event.pathParameters.id
     }
   }
 
   try {
-    await dynamoDbLib.call('put', params)
-    return success(params.Item)
+    const result = await dynamoDbLib.call('get', params)
+
+    if (result.Item) {
+      return success(result.Item)
+    } else {
+      return failure({ status: false, error: 'Item not found' })
+    }
   } catch (e) {
     return failure({ status: false })
   }
